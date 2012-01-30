0ebc77111d96cad056980153302f89663a956700
Simo Sorce <simo@redhat.com>
2012-01-29 22:03:44 -0500

    Move some definitions around.
    Make union arg/res usable elsewhere.
    Make gp_xdr_set private as gp_rpc_process.c is the only place where it is used.

--- src/gp_rpc_process.h
+++ src/gp_rpc_process.h
@@ -39,47 +39,9 @@
 
 struct gssproxy_ctx;
 
-union gp_rpc_arg {
-    gssx_arg_release_handle release_handle;
-    gssx_arg_indicate_mechs indicate_mechs;
-    gssx_arg_import_and_canon_name import_and_canon_name;
-    gssx_arg_get_call_context get_call_context;
-    gssx_arg_acquire_cred acquire_cred;
-    gssx_arg_export_cred export_cred;
-    gssx_arg_import_cred import_cred;
-    gssx_arg_store_cred store_cred;
-    gssx_arg_init_sec_context init_sec_context;
-    gssx_arg_accept_sec_context accept_sec_context;
-    gssx_arg_get_mic get_mic;
-    gssx_arg_verify_mic verify_mic;
-    gssx_arg_wrap wrap;
-    gssx_arg_unwrap unwrap;
-    gssx_arg_wrap_size_limit wrap_size_limit;
-};
-
-union gp_rpc_res {
-    gssx_res_release_handle release_handle;
-    gssx_res_indicate_mechs indicate_mechs;
-    gssx_res_import_and_canon_name import_and_canon_name;
-    gssx_res_get_call_context get_call_context;
-    gssx_res_acquire_cred acquire_cred;
-    gssx_res_export_cred export_cred;
-    gssx_res_import_cred import_cred;
-    gssx_res_store_cred store_cred;
-    gssx_res_init_sec_context init_sec_context;
-    gssx_res_accept_sec_context accept_sec_context;
-    gssx_res_get_mic get_mic;
-    gssx_res_verify_mic verify_mic;
-    gssx_res_wrap wrap;
-    gssx_res_unwrap unwrap;
-    gssx_res_wrap_size_limit wrap_size_limit;
-};
-
 #define gp_exec_std_args struct gssproxy_ctx *gpctx, \
                          union gp_rpc_arg *arg, \
                          union gp_rpc_res *res
-
-typedef int (*gp_exec_fn)(gp_exec_std_args);
 
 int gp_indicate_mechs(gp_exec_std_args);
 int gp_get_call_context(gp_exec_std_args);
@@ -96,11 +58,5 @@ int gp_verify(gp_exec_std_args);
 int gp_wrap(gp_exec_std_args);
 int gp_unwrap(gp_exec_std_args);
 int gp_wrap_size_limit(gp_exec_std_args);
-
-struct gp_rpc_fn_set {
-    xdrproc_t arg_fn;
-    xdrproc_t res_fn;
-    gp_exec_fn exec_fn;
-};
 
 #endif /* _GP_RPC_PROCESS_H_ */
