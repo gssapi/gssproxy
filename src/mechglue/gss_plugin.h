30b14bfef8c1d8e998428355a20ae3fcb81cec2d
Simo Sorce <simo@redhat.com>
2012-05-31 18:49:23 -0400

    Implement cred related mechglue wrappers

--- src/mechglue/gss_plugin.h
+++ src/mechglue/gss_plugin.h
@@ -68,4 +68,78 @@ uint32_t gpp_name_to_local(uint32_t *minor, gssx_name *name,
 uint32_t gpp_local_to_name(uint32_t *minor,
                            gss_name_t local_name, gssx_name **name);
 
+OM_uint32 gssi_acquire_cred(OM_uint32 *minor_status,
+                            const gss_name_t desired_name,
+                            OM_uint32 time_req,
+                            const gss_OID_set desired_mechs,
+                            gss_cred_usage_t cred_usage,
+                            gss_cred_id_t *output_cred_handle,
+                            gss_OID_set *actual_mechs,
+                            OM_uint32 *time_rec);
+
+OM_uint32 gssi_add_cred(OM_uint32 *minor_status,
+                        const gss_cred_id_t input_cred_handle,
+                        const gss_name_t desired_name,
+                        const gss_OID desired_mech,
+                        gss_cred_usage_t cred_usage,
+                        OM_uint32 initiator_time_req,
+                        OM_uint32 acceptor_time_req,
+                        gss_cred_id_t *output_cred_handle,
+                        gss_OID_set *actual_mechs,
+                        OM_uint32 *initiator_time_rec,
+                        OM_uint32 *acceptor_time_rec);
+
+OM_uint32 gssi_acquire_cred_with_password(OM_uint32 *minor_status,
+                                          const gss_name_t desired_name,
+                                          const gss_buffer_t password,
+                                          OM_uint32 time_req,
+                                          const gss_OID_set desired_mechs,
+                                          gss_cred_usage_t cred_usage,
+                                          gss_cred_id_t *output_cred_handle,
+                                          gss_OID_set *actual_mechs,
+                                          OM_uint32 *time_rec);
+
+OM_uint32 gppint_get_def_creds(OM_uint32 *minor_status,
+                               enum gpp_behavior behavior,
+                               struct gpp_name_handle *name,
+                               gss_cred_usage_t cred_usage,
+                               struct gpp_cred_handle **cred_handle);
+
+OM_uint32 gssi_inquire_cred(OM_uint32 *minor_status,
+                            gss_cred_id_t cred_handle,
+                            gss_name_t *name,
+                            OM_uint32 *lifetime,
+                            gss_cred_usage_t *cred_usage,
+                            gss_OID_set *mechanisms);
+
+OM_uint32 gssi_inquire_cred_by_mech(OM_uint32 *minor_status,
+                                    gss_cred_id_t cred_handle,
+                                    gss_OID mech_type,
+                                    gss_name_t *name,
+                                    OM_uint32 *initiator_lifetime,
+                                    OM_uint32 *acceptor_lifetime,
+                                    gss_cred_usage_t *cred_usage);
+
+OM_uint32 gssi_inquire_cred_by_oid(OM_uint32 *minor_status,
+	                           const gss_cred_id_t cred_handle,
+	                           const gss_OID desired_object,
+	                           gss_buffer_set_t *data_set);
+
+OM_uint32 gssi_set_cred_option(OM_uint32 *minor_status,
+                               gss_cred_id_t cred_handle,
+                               const gss_OID desired_object,
+                               const gss_buffer_t value);
+
+OM_uint32 gssi_store_cred(OM_uint32 *minor_status,
+                          const gss_cred_id_t input_cred_handle,
+                          gss_cred_usage_t input_usage,
+                          const gss_OID desired_mech,
+                          OM_uint32 overwrite_cred,
+                          OM_uint32 default_cred,
+                          gss_OID_set *elements_stored,
+                          gss_cred_usage_t *cred_usage_stored);
+
+OM_uint32 gssi_release_cred(OM_uint32 *minor_status,
+                            gss_cred_id_t *cred_handle);
+
 #endif /* _GSS_PLUGIN_H_ */
