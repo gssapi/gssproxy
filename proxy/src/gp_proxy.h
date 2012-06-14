0db9885a0217bd9416c464f26a1caa7d33fcaa9a
Günther Deschner <gdeschner@redhat.com>
2012-06-14 16:57:15 +0200

    Add gp_service_get_ring_buffer to retrieve buffer from a service.
    Guenther
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- proxy/src/gp_proxy.h
+++ proxy/src/gp_proxy.h
@@ -82,6 +82,7 @@ struct gp_conn;
 
 /* from gp_config.c */
 struct gp_config *read_config(char *config_file, int opt_daemonize);
+struct gp_ring_buffer *gp_service_get_ring_buffer(struct gp_service *svc);
 
 /* from gp_init.c */
 void init_server(bool daemonize);
