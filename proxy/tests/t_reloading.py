40729d65b10242d7c76691988994e673dcfad598
Robbie Harwood <rharwood@redhat.com>
2016-12-07 20:15:14 +0000

    Indent subtests
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>
    Reviewed-by: Simo Sorce <simo@redhat.com>

--- proxy/tests/t_reloading.py
+++ proxy/tests/t_reloading.py
@@ -9,12 +9,14 @@ def run(testdir, env, basicconf):
     keysenv = basicconf["keysenv"]
 
     print("Testing basic SIGHUP with no change", file=sys.stderr)
+    sys.stderr.write("  ")
     basicconf['prefix'] += prefix + "_1"
     os.kill(basicconf["gpid"], signal.SIGHUP)
     time.sleep(1) #Let gssproxy reload everything
     run_basic_test(testdir, env, basicconf)
 
     print("Testing SIGHUP with dropped service", file=sys.stderr)
+    sys.stderr.write("  ")
     basicconf['prefix'] = prefix + "_2"
     update_gssproxy_conf(testdir, keysenv, GSSPROXY_CONF_MINIMAL_TEMPLATE)
     os.kill(basicconf["gpid"], signal.SIGHUP)
@@ -22,6 +24,7 @@ def run(testdir, env, basicconf):
     run_basic_test(testdir, env, basicconf, True)
 
     print("Testing SIGHUP with new service", file=sys.stderr)
+    sys.stderr.write("  ")
     basicconf['prefix'] = prefix + "_3"
     update_gssproxy_conf(testdir, keysenv, GSSPROXY_CONF_TEMPLATE)
     os.kill(basicconf["gpid"], signal.SIGHUP)
@@ -29,6 +32,7 @@ def run(testdir, env, basicconf):
     run_basic_test(testdir, env, basicconf)
 
     print("Testing SIGHUP with change of socket", file=sys.stderr)
+    sys.stderr.write("  ")
     basicconf['prefix'] = prefix + "_4"
     update_gssproxy_conf(testdir, keysenv, GSSPROXY_CONF_SOCKET_TEMPLATE)
     env['GSSPROXY_SOCKET'] += "2"
